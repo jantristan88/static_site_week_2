87be791e688eedd7d8bf02b813a0846c9a65c58e
Jan Tristan <jan.tristan@gmail.com>
2019-02-10 23:45:53 -0800

    Combined all templates and content for three sections

--- build.py
+++ build.py
@@ -1,7 +1,14 @@
-fragment_a = open('templates/Top.html').read()
-fragment_b = open('content/index.html').read()
-fragment_c = open('templates/Bottom.html').read()
+top_template = open('templates/top_template.html').read()
+bottom_template = open('templates/bottom_template.html').read()
 
-home = fragment_a + fragment_b + fragment_c
+index = open('content/index.html').read()
+index_html = top_template + home + bottom_template
+open('index.html', 'w+').write(index_html)
 
-print(home)
+FeatureProject= open('content/FeatureProject.html').read()
+FeatureProject_html = top_template + FeatureProject + bottom_template
+open('FeatureProject.html', 'w+').write(FeaturedProject_html)
+
+Publication = open('content/Publication.html').read()
+Publication_html = top_template + content + bottom_template
+open('Publication.html', 'w+').write(Publication_html)
