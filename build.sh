8524198e6f2d8c4255374b7fd49337be4eda4bb0
Jan Tristan <jan.tristan@gmail.com>
2019-02-11 17:59:45 -0800

    Create build.sh, overwrites docs files that were created earlier, forgot to save the bash file

---
+++ build.sh
@@ -0,0 +1,5 @@
+cat templates/Top.html content/index.html templates/Bottom.html > docs/index.html
+
+cat templates/Top.html content/FeatureProject.html templates/Bottom.html > docs/FeatureProject.html
+
+cat templates/Top.html content/Publication.html templates/Bottom.html > docs/Publication.html
