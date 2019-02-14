7cb80bb95535f6626f7efa4350bf8f77fc6efb0b
Jan Tristan <jan.tristan@gmail.com>
2019-02-13 18:36:28 -0800

    correct path

--- build.py
+++ build.py
@@ -3,12 +3,12 @@ bottom_template = open('templates/bottom_template.html').read()
 
 index = open('content/index.html').read()
 index_html = top_template + home + bottom_template
-open('index.html', 'w+').write(index_html)
+open('docs/index.html', 'w+').write(index_html)
 
 FeatureProject= open('content/FeatureProject.html').read()
 FeatureProject_html = top_template + FeatureProject + bottom_template
-open('FeatureProject.html', 'w+').write(FeaturedProject_html)
+open('docs/FeatureProject.html', 'w+').write(FeaturedProject_html)
 
 Publication = open('content/Publication.html').read()
 Publication_html = top_template + content + bottom_template
-open('Publication.html', 'w+').write(Publication_html)
+open('docs/Publication.html', 'w+').write(Publication_html)
