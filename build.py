2ceaf224471bca2472a6f8ce4fa565ce5dfd902b
Jan Tristan <jan.tristan@gmail.com>
2019-02-13 18:40:42 -0800

    changed content name to direct to correct link

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ top_template = open('templates/top_template.html').read()
 bottom_template = open('templates/bottom_template.html').read()
 
 index = open('content/index.html').read()
-index_html = top_template + home + bottom_template
+index_html = top_template + index + bottom_template
 open('docs/index.html', 'w+').write(index_html)
 
 FeatureProject= open('content/FeatureProject.html').read()
@@ -10,5 +10,5 @@ FeatureProject_html = top_template + FeatureProject + bottom_template
 open('docs/FeatureProject.html', 'w+').write(FeaturedProject_html)
 
 Publication = open('content/Publication.html').read()
-Publication_html = top_template + content + bottom_template
+Publication_html = top_template + Publication + bottom_template
 open('docs/Publication.html', 'w+').write(Publication_html)
